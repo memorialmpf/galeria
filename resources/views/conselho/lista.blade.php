f4b2fabb10866b06dc1c6bb906f0ad62fa582bdb
Abraão Pessoa <Abrao Pessoa@DESKTOP-BBMS8BJ>
2018-09-14 16:52:26 -0300

    UI Layout Structure started

---
+++ resources/views/conselho/lista.blade.php
@@ -0,0 +1,279 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/cover.css">
+
+    <title>Galeria Membros 2</title>
+  </head>
+    <body class="text-center">
+
+      <div class="d-flex w-100 h-100 mx-auto flex-column">
+        <header class="masthead">
+          <div class="text-center mt-2">
+            <a href="index.html"><img src="img/bemvindo-big.png" width="360" alt=""></a>
+          </div>
+        </header>
+
+        <!-- LINHA COM AS COLUNAS -->
+        <div class="row w-100 max-auto align-items-start">
+
+          <!-- COLUNA NAVEGACAO 1 -->
+          <div class="col-1">
+            <img src="img/tarja-1on.png" alt="">
+          </div>
+
+          <!-- COLUNA MEMBROSO -->
+          <div class="col-9" style="overflow-y:scroll; height: 900px;">
+            <div class="row">
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-k.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-k.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <a href="detalhe.html"><img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt=""></a>
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-k.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-k.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Aristides Junqueira Alvarenga</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-x.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-k.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-y.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-z.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+                <div class="col">
+                  <img class="img-fluid rounded-circle" src="img/membro-w.jpg" width="120" height="120" alt="">
+                  <p class="small-nome">Lorem Ipsom Dolorem</p>
+                </div>
+            </div>
+
+          </div>
+
+        <!-- COLUNA NAVEGACAO 2 -->
+
+
+              <div class="col-1">
+                <div class="linha-vertical"><img src="img/tarja-2off.png" alt=""  class="btn"></div>
+
+              </div>
+
+              <div class="col-1">
+                <div class="linha-vertical"><img src="img/tarja-3off.png" alt="" class="btn"></div>
+
+              </div>
+
+
+
+
+
+        </div>
+        <!-- FOOTER LOGO MEMORIAL-->
+        <footer class="mastfoot mt-auto align-middle align-items-middle" style="background-color: #a61c1f !important; height: 60px;">
+          <div class="inner">
+            <img src="img/logo-memorial-mpf.png" class="align-middle mt-2" alt="">
+          </div>
+        </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+
+
+
+</div>
---
+++ resources/views/index.html
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/cover.css">
+
+    <title>Galeria Membros 1</title>
+  </head>
+    <body class="text-center">
+
+      <div class="d-flex w-100 h-100 mx-auto flex-column">
+        <header class="masthead mb-auto">
+          <div class="text-center mt-4">
+            <img src="img/bemvindo-big.png" alt="">
+          </div>
+        </header>
+
+          <!-- <h1 class="cover-heading">Cover your page.</h1>
+          <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+          <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+          </p> -->
+
+                 <div class="row w-100 mx-auto">
+                   <div class="col-lg-4 pl-4">
+                     <!-- <div class="rounded-circle btn ball align-middle" src="img/btn-galeria-1.jpg" style="width: 540px; height:540px;"></div> -->
+                     <a href="lista.html"><img class="img-fluid rounded-circle btn ball" src="img/btn-galeria-1.jpg" width="540" height="540"></a>
+                   </div><!-- /.col-lg-4 -->
+                   <div class="col-lg-4">
+                     <a href="lista.html"><img class="img-fluid rounded-circle btn ball" src="img/btn-galeria-2.jpg" width="540" height="540"></a>
+                   </div>
+                   <div class="col-lg-4 pr-4">
+                     <a href="lista.html"><img class="img-fluid rounded-circle btn ball" src="img/btn-galeria-3.jpg" width="540" height="540"></a>
+                   </div>
+                 </div>
+
+
+        <footer class="mastfoot mt-auto align-middle align-items-middle" style="background-color: #a61c1f !important; height: 60px;">
+          <div class="inner">
+            <img src="img/logo-memorial-mpf.png" class="align-middle mt-2" alt="">
+          </div>
+        </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+
+
+
+</div>
