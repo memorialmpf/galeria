67e69326b1d4899a95ff133ccdfaf943d51f9813
Abraão Pessoa <abraaopessoa@yahoo.com.br>
2018-11-29 13:22:38 -0200

    Subs com desde

--- resources/views/detalhe.blade.php
+++ resources/views/detalhe.blade.php
@@ -241,10 +241,17 @@
             <p>{{$membro->pess_nm}}</p>
             <hr>
             <p class="membro-detalhe-titulo">Período</p>
- <p class="membro-detalhe-texto">{{$membro->ceft_ds}} de  {{date('d/m/Y', strtotime($membro->pess_dt_posse))}} até  {{(!is_null($membro->pess_dt_desliga))?date('d/m/Y', strtotime($membro->pess_dt_desliga)):" atualmente"}} </p>
+ <p class="membro-detalhe-texto">
 
 
+  {{$membro->ceft_ds}}
+@if(is_null($membro->pess_dt_desliga))
+desde  {{date('d/m/Y', strtotime($membro->pess_dt_posse))}}
+@else
+de {{date('d/m/Y', strtotime($membro->pess_dt_posse))}} até  {{date('d/m/Y', strtotime($membro->pess_dt_desliga))}}
+@endif
 
+ </p>
 
 
 
